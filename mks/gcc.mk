5c152d22110acddf3ed8862e4f4bd088cd8179e1
francovaro <francovaro@gmail.com>
2025-07-24 10:01:40 +0200

    moved startup code

--- mks/gcc.mk
+++ mks/gcc.mk
@@ -1,7 +1,7 @@
 C_SOURCES_GCC += Gcc/Src/console.c
 C_SOURCES_GCC += Gcc/Src/syscalls.c
 
-ASM_SOURCES_GCC += $(FW_REL_DIR)/Drivers/CMSIS/Device/ST/STM32N6xx/Source/Templates/gcc/startup_stm32n657xx.s
+ASM_SOURCES_GCC += Gcc/Src/startup_stm32n657xx.s
 
 C_SOURCES += $(C_SOURCES_GCC)
 ASM_SOURCES += $(ASM_SOURCES_GCC)
